c53b52b0883cab09ec628bcc53669639208ba9e4
Jair Delval <a355274@uach.mx>
2023-10-31 21:27:37 -0600

    10. Introducir un número por teclado y que nos diga si es par o impar.

---
+++ 10.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Introduce un número: ', (numero) => {
+    numero = parseInt(numero);
+    if (numero% 2 == 0){
+        console.log("Es par");
+    } else (console.log("Es impar"));
+    rl.close();
+  });
