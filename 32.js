bdf28b06c68b0ea463cf6c4f8fc79ce1301d7b72
Jair Delval <a355274@uach.mx>
2023-11-02 11:26:47 -0600

    32. Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta

---
+++ 32.js
@@ -0,0 +1,26 @@
+const personas = [
+    'Jair',
+    'Christian',
+    'Hilda',
+    'Diana',
+    'Daniel',
+    'Anette',
+    'David',
+    'Yoselin',
+    'Carlos',
+    'Taysha',
+    'Pepe',
+    'Francisco',
+    'Salvador',
+    'Victor',
+    'Alan',
+    'Jose',
+    'Adrian',
+    'Adriana',
+    'Ana',
+    'Carla',
+  ];
+  
+  const listaPersonas = personas.join('\n');
+  console.log(listaPersonas);
+  
